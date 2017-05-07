4d589fa7509fc5f51c9b42fc6970b53d913953ee
Marcin Chrzanowski <marcin.j.chrzanowski@gmail.com>
2017-05-08 00:19:51 +0200

    Implement sending and reception of client messages

--- socket.h
+++ socket.h
@@ -3,10 +3,11 @@
 
 #include <exception>
 
-#include <netinet/in.h>
-#include <sys/types.h>
 #include <sys/socket.h>
 #include <unistd.h>
+
+#include "address.h"
+#include "message.h"
 
 class AddressNotAvailable : std::exception {};
 
@@ -19,17 +20,12 @@ public:
         }
     }
 
-    Socket(int port) : Socket() {
-        address_ = {
-            .sin_family = AF_INET,
-            .sin_port = htons(port),
-            .sin_addr = {
-                .s_addr = htonl(INADDR_ANY)
-            }
-        };
+    Socket(in_port_t port) : Socket() {
+        address_ = Address(htons(port));
 
-        int rv = bind(fd_, (struct sockaddr *) &address_,
-                (socklen_t) sizeof(address_));
+        struct sockaddr_in my_addr = address_.create_sockaddr_struct();
+        int rv = bind(fd_, (struct sockaddr *) &my_addr,
+                (socklen_t) sizeof(my_addr));
 
         if (rv < 0) {
             if (errno == EALREADY || errno == EADDRINUSE ||
@@ -43,9 +39,31 @@ public:
         close(fd_);
     }
 
+    void send(ClientMessage &message, Address address) {
+        uint8_t buffer[9];
+        message.to_bytes(buffer);
+
+        struct sockaddr_in dest_addr = address.create_sockaddr_struct();
+
+        sendto(fd_, buffer, 9, 0, (struct sockaddr *)&dest_addr,
+                sizeof(dest_addr));
+    }
+
+    std::pair<ClientMessage, Address> receiveFromClient() {
+        struct sockaddr_in client_addr;
+        uint8_t buffer[9];
+        socklen_t addr_length = sizeof(client_addr);
+        recvfrom(fd_, buffer, 9, 0, (struct sockaddr *)&client_addr,
+                &addr_length);
+
+        in_addr_t address = client_addr.sin_addr.s_addr;
+        in_port_t port = client_addr.sin_port;
+        return std::make_pair(ClientMessage(buffer), Address(address, port));
+    }
+
 private:
     int fd_;
-    struct sockaddr_in address_;
+    Address address_;
 };
 
 #endif
--- test_socket.cc
+++ test_socket.cc
@@ -1,5 +1,11 @@
+#include <string>
+
+#include <netdb.h>
+#include <netinet/in.h>
+
 #include "testing.h"
 #include "socket.h"
+#include "message.h"
 
 void test_constructor() {
     begin_test();
@@ -14,6 +20,46 @@ void test_constructor() {
     check_no_exception_thrown([] { Socket s; }, "Without argument.");
 }
 
+void test_client_message_communication() {
+    begin_test();
+
+    Socket s1(2222);
+    Socket s2(2223);
+
+    Address destination("localhost", htons(2223));
+    ClientMessage to_send(0x42, 'c');
+    s1.send(to_send, destination);
+
+    std::pair<ClientMessage, Address> received = s2.receiveFromClient();
+    ClientMessage message = received.first;
+    Address address = received.second;
+
+    ClientMessage to_send2(0x420, 'd');
+    s2.send(to_send2, address);
+    std::pair<ClientMessage, Address> received2 = s1.receiveFromClient();
+
+    ClientMessage message2 = received2.first;
+    Address address2 = received2.second;
+
+    check_equal<uint64_t>(message.timestamp, 0x42,
+            "Got the correct timestamp.");
+    check_equal<char>(message.character, 'c',
+            "Got the correct character.");
+    check_equal<unsigned>(ntohs(address.port), 2222,
+            "Got the correct sender port.");
+    check_equal<uint64_t>(message2.timestamp, 0x420,
+            "Got the correct timestamp.");
+    check_equal<char>(message2.character, 'd',
+            "Got the correct character.");
+    check_equal<unsigned>(ntohs(address2.port), 2223,
+            "Got the correct sender port.");
+}
+
+void test_server_message_communication() {
+    begin_test();
+}
+
 int main() {
     test_constructor();
+    test_client_message_communication();
 }
